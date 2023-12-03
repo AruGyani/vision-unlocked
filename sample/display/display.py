63c7b89f6dd8036d91a056e7e38cb6f657fc84f6
Aru Gyani <gyaniaru@gmail.com>
2023-12-02 19:30:48 -0600

    feat: tts and ui

--- sample/display/display.py
+++ sample/display/display.py
@@ -1,16 +1,35 @@
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import *
+from gtts import gTTS
 
 import sys
+import os
 
 class Widget(QWidget):
   def __init__(self, parent=None):
     super(Widget, self).__init__(parent)
 
-    push_button = QPushButton("Hello")
+    self.setWindowTitle("vision unLocked")
 
     layout = QGridLayout()
     self.setLayout(layout)
+
+    self.hello = QPushButton('Hello')
+    self.hello.clicked.connect(lambda: self.greetings("Hello"))
+
+    self.world = QPushButton('World')
+    self.world.clicked.connect(lambda: self.greetings('World'))
+
+    layout.addWidget(self.hello, 0, 0,
+                     alignment=Qt.AlignmentFlag.AlignTop)
+    layout.addWidget(self.world, 1, 0,
+                     alignment=Qt.AlignmentFlag.AlignBottom)
+
+  def greetings(self, string):
+    speech = gTTS(text=string, lang='en', slow=False)
+    speech.save("text.mp3")
+    os.system("afplay text.mp3")
+    
 
 if __name__ == "__main__":
   app = QApplication()
